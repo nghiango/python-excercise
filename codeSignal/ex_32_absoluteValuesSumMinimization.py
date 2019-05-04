8a6e840ddd8cb325f77335eeda6c074b6c902e33
Nghia Ngo <tinhthien30@gmail.com>
2019-05-04 17:24:00 +0700

    Solve excercise 32

--- codeSignal/ex_32_absoluteValuesSumMinimization.py
+++ codeSignal/ex_32_absoluteValuesSumMinimization.py
@@ -1,7 +1,29 @@
 # https://app.codesignal.com/arcade/intro/level-7/ZFnQkq9RmMiyE6qtq
+import sys
 def absoluteValuesSumMinimization(a):
-    print("Todo")
+    correctValue = sys.maxsize
+    minSum = sys.maxsize
+    for value in a:
+        sumAbs = sumAbsOfOneElement(a, value)
+        if minSum > sumAbs:
+            minSum = sumAbs
+            correctValue = value
+        elif minSum == sumAbs and correctValue > value:
+            correctValue = value
+    return correctValue
+    # return a[(len(a)-1)//2] This is the advance version
 
 
-a = [2, 4, 7]
-absoluteValuesSumMinimization(a)
+def absBetweenTwoNumber(first, second):
+    return abs(first - second)
+
+
+def sumAbsOfOneElement(array, element):
+    acc = 0
+    for value in array:
+        acc += absBetweenTwoNumber(value, element)
+    return acc
+
+
+a = [2, 4, 7, 8, 9]
+print(absoluteValuesSumMinimization(a))
