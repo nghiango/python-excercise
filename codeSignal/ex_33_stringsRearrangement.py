0355896ab5ca1bb066148030580497085eb37b1a
nghia.ngo <nghia.ngo.22.11@gmail.com>
2019-08-08 08:34:33 +0700

    codeSignal | exercise 33

--- codeSignal/ex_33_stringsRearrangement.py
+++ codeSignal/ex_33_stringsRearrangement.py
@@ -1,3 +1,16 @@
 # https://app.codesignal.com/arcade/intro/level-7/PTWhv2oWqd6p4AHB9
 def stringsRearrangement(inputArray):
     print("")
+
+
+def isDifferentByOneCharacter(string, otherString):
+    acc = 0
+    for i in range(len(string)):
+        if string[i] != otherString[i]:
+            acc += 1
+        if acc > 1:
+            return False
+    return True
+
+print(isDifferentByOneCharacter("aaa", "aac"))
+print(isDifferentByOneCharacter("aaa", "cac"))
