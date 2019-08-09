1fe8c987619cd808134c93d0e8a59e9ac2b5da0e
nghia.ngo <nghia.ngo.22.11@gmail.com>
2019-08-09 08:18:38 +0700

    Update exercise 33

--- codeSignal/ex_33_stringsRearrangement.py
+++ codeSignal/ex_33_stringsRearrangement.py
@@ -1,16 +1,42 @@
 # https://app.codesignal.com/arcade/intro/level-7/PTWhv2oWqd6p4AHB9
-def stringsRearrangement(inputArray):
-    print("")
-
+def stringsRearrangement(inputArray: list):
+    hasAnySatisfy = False
+    for i in range(len(inputArray)):
+        for j in range(len(inputArray)):
+            tempList = inputArray.copy()
+            tempList.pop(i)
+            tempList.insert(j, inputArray[i])
+            hasAnySatisfy = isAllElementsDifferByOneCharacter(tempList)
+            print(tempList)
+            if hasAnySatisfy:
+                return True
+    return False
 
 def isDifferentByOneCharacter(string, otherString):
     acc = 0
     for i in range(len(string)):
         if string[i] != otherString[i]:
             acc += 1
-        if acc > 1:
-            return False
+    if acc == 1:
+        return True
+    return False
+
+def isAllElementsDifferByOneCharacter(arr: list):
+    isSatisfyAll = True
+    for i in range(len(arr)):
+        if (i+1) < len(arr):
+            isSatisfyAll = isDifferentByOneCharacter(arr[i], arr[i+1])
+            if isSatisfyAll == False:
+                return False
     return True
 
-print(isDifferentByOneCharacter("aaa", "aac"))
-print(isDifferentByOneCharacter("aaa", "cac"))
+def main():
+    stringarr = ["abc", 
+ "bef", 
+ "bcc", 
+ "bec", 
+ "bbc", 
+ "bdc"]
+    print(stringsRearrangement(stringarr))
+
+main()
