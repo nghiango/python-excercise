7f3d45881ce77297d4003f7aa42f68189bc00c2e
Nghia Ngo <nghia.ngo@ubitec.ch>
2019-08-15 16:37:07 +0700

    Finish exercise 34

--- python/codeSignal/ex_34_extractEachKth.py
+++ python/codeSignal/ex_34_extractEachKth.py
@@ -1,3 +1,15 @@
+# https://app.codesignal.com/arcade/intro/level-8/3AgqcKrxbwFhd3Z3R
 from shared.utils import *
 
-print_result('does it word')
+
+def extractEachKth(inputArray: list, k: int):
+    # We use `del` keyword to remove
+    # and `slicing` in python to get sub-list
+    # before first colon is the start index
+    # after first colon is the end index
+    # after second colon is the increment
+    del inputArray[k-1::k]
+    return inputArray
+
+
+print(extractEachKth([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3))
