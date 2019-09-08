7554fb1f8636d47be7500bc7be8ddd1fc6041f12
nghia.ngo <nghia.ngo.22.11@gmail.com>
2019-09-08 16:23:54 +0700

    Finish exercise 42

--- python/codeSignal/ex_42_bishopAndPawn.py
+++ python/codeSignal/ex_42_bishopAndPawn.py
@@ -1,5 +1,24 @@
 # https://app.codesignal.com/arcade/intro/level-9/6M57rMTFB9MeDeSWo
 
 
+class Location:
+    def __init__(self, location):
+        self.location = location
+        self.char_location = location[0]
+        self.num_location = location[1]
+
+
 def bishopAndPawn(bishop, pawn):
-    print('hello')
+    location_bishop = Location(bishop)
+    location_pawn = Location(pawn)
+
+    num_distance = abs(int(location_bishop.num_location) - int(location_pawn.num_location))
+    print(num_distance)
+    if (ord(location_bishop.char_location) + num_distance) == ord(location_pawn.char_location):
+        return True
+    if (ord(location_bishop.char_location) - num_distance) == ord(location_pawn.char_location):
+        return True
+    return False
+
+
+print(bishopAndPawn('a1', 'b3'))
---
+++ python/otherProblems/dynamicPrograming/maxSubOfIncreaseAttribute.py
@@ -0,0 +1,2 @@
+
+print('Xin chao')
